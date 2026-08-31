4205f8e8c6111423e3f5e1efb27651cc5d105107
omar <omarmoharam790@gmail.com>
2026-08-31 21:56:34 +0300

    feat: implement Task 3.3 - IoC with Annotations

---
+++ src/com/shapesWithAnnotations/Circle.java
@@ -0,0 +1,21 @@
+package com.shapesWithAnnotations;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Circle implements Shape {
+    private double radius;
+
+    @Override
+    public double getArea() {
+        return (Math.PI * (Math.pow(radius, 2)));
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
