5e42fdf9d2ae6ebb87c2cb1432b6660d79f528ea
omar <omarmoharam790@gmail.com>
2026-08-31 21:56:35 +0300

    feat: implement Task 5 - User Profile System Spring MVC application

---
+++ src/com/profile/model/ProfileModel.java
@@ -0,0 +1,54 @@
+package com.profile.model;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import java.util.Date;
+
+public class ProfileModel {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String city;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date dateOfBirth;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
